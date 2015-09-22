bf7fc9dbd94c92ccc1504eb7ee9400c1f4b2e6b4
SageLam <48594138@qq.com>
2015-09-22 11:02:58 +0800

    wrote a test

---
+++ test.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int *time = new int [3];
+	int *number = new int [3];
+	int num[20], n;
+	cin>>n;
+	for(int i = 0; i < n; i++)
+		cin>>num[i];
+	for(int i = 0; i < n; i++)
+	{
+		int j;
+		for(j = 0; j < 3; j++)
+		{
+			if(number[j] == num[i])
+			{
+				time[j]++;
+				break;
+			}
+		}
+		if(j == 3)
+		for(j = 0; j < 3; j++)
+		{
+			if(time[j] == 0)
+			{
+				time[j] = 1;
+				number[j] = num[i];
+				break;
+			}
+		}
+		if(j == 3)
+		{
+			for(j = 0; j < 3; j++)
+				time[j]--;
+		}
+	}
+	for(int i = 0; i < 3; i++)
+	{
+		cout<<number[i]<<'\t';
+	}
+	cout<<endl;
+}
